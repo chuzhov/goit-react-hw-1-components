637bd976b3c5560ef034fe39cf2734e3268676a0
Dan Chuzhov <dan.chuzhov.htc@gmail.com>
2022-12-15 20:28:46 +0200

    Finishes markup

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -4,23 +4,30 @@ import css from "./FriendList.module.css"
 
 const FriendList = ({ friends }) => {
     
-    const friendListMarkup = friends.map(friend => 
-        <FriendItem key={friend.id}
-                    avatar={friend.avatar} 
-                    isOnline={friend.isOnline} 
-                    name={friend.name} />
-        );
     return (
         <section className="friends">
             <ul className={css["friend-list"]}>
-                {friendListMarkup}
+                { friends.map(friend => (
+                    <FriendItem key={friend.id}
+                                avatar={friend.avatar} 
+                                isOnline={friend.isOnline} 
+                                name={friend.name} />
+                    )
+                )}
             </ul>
         </section>
     )
 }
 
 FriendList.propTypes = {
-    friends: PropTypes.array.isRequired,
+    friends: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            avatar: PropTypes.string.isRequired,
+            isOnline: PropTypes.bool.isRequired,
+            name: PropTypes.string.isRequired
+        })
+    )
 }
 
 export default FriendList;
